e932659b5a693bdc2fcb31db3ef5f8769c049944
K7 667 <k7.667.k7@gmail.com>
2023-12-20 14:59:33 +0100

    exo | adapter ts 1 & 2

---
+++ exo/adapter/exercise-1.ts
@@ -0,0 +1,32 @@
+class AdaptateurFourABois implements IFourElectrique {
+    private fourABois: FourABois;
+  
+    constructor(fourABois: FourABois) {
+        this.fourABois = fourABois;
+    }
+  
+    ouvrir(porte: Number): Promise<Boolean> {
+        return this.fourABois.deverouiller();
+    }
+  
+    cuire(pizza: IPizza): Promise<Boolean> {
+        return Promise.all([
+            this.fourABois.enclencher(1),
+            this.fourABois.placer(pizza),
+            this.fourABois.faireTourner(),
+            this.fourABois.chaleur(),
+        ]).then((results) => results.every((result) => result === true));
+    }
+}
+  
+  // Utilisation de l'adaptateur
+  console.log("Préparation d'une pizza !");
+  const pizza = new Pizza('marguerita', 7);
+  
+  const voiceCommander = new VoiceCommand();
+  const fourABois = new FourABois(voiceCommander);
+  
+  const adaptateur = new AdaptateurFourABois(fourABois);
+  
+  adaptateur.ouvrir(1).then(() => adaptateur.cuire(pizza));
+  
---
+++ exo/adapter/exercise-2.ts
@@ -0,0 +1,33 @@
+class AdapterApi implements ICodeBarreApi {
+    constructor(private newApi: NewCodeBarreApi) {}
+  
+    makeRequest(barreCode: string): Promise<IProduit> {
+      return this.newApi.fetch(barreCode)
+        .then((newApiProduct: INewApiProduit) => {
+          return {
+            prix: newApiProduct.data.price,
+            quantite: newApiProduct.data.quantity,
+            poids: newApiProduct.data.weight,
+            nutriscore: newApiProduct.data.quality.nutriscore,
+            taille: this.parseSize(newApiProduct.data.size),
+          };
+        });
+    }
+  
+    private parseSize(sizeString: string): { largeur: number, hauteur: number, profondeur: number } {
+      const [largeur, hauteur, profondeur] = sizeString.split('x').map(Number);
+      return { largeur, hauteur, profondeur };
+    }
+  }
+  
+  // Utilisation de l'adaptateur
+  console.log('Je vais chercher le produit BE49RJ2UFR sur l\'api');
+  
+  const newApi = new NewCodeBarreApi();
+  const apiCodeBarre: ICodeBarreApi = new AdapterApi(newApi);
+  
+  apiCodeBarre.makeRequest("BE49RJ2UFR")
+    .then((product) => {
+      console.log(product.prix, product.nutriscore, product.taille.hauteur);
+    });
+  
